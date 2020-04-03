d92de26221988ddb50910389c2ceffe744924d96
Christopher Reinken <christopherreinken@gmail.com>
2020-04-03 15:42:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,18 @@
 class Anagram
   
+  attr_accessor :anagram_test
+  
+  def initialize(word)
+    @anagram_test = word
+  end
+  
+  def match(test_array)
+    test_array.each do |test_word|
+      a = test_word.split("")
+      b = @anagram_test.split("")
+      
+      a.sort == b.sort ? true : false
+    end
+  end
+  
 end
